913e66bdc88242acb2f7bf733c976cf685b695c0
Dan Conlon <dan@dansplace.co.uk>
2023-03-01 19:16:31 +0000

    Interfacer to fetch data from Sofar solar inverter

--- src/interfacers/__init__.py
+++ src/interfacers/__init__.py
@@ -29,6 +29,7 @@ __all__ = [
     "EmonHubBleInterfacer",
     "EmonHubGoodWeInterfacer",
     "EmonHubInfluxInterfacer",
-    "EmonHubHeatmiserInterfacer"
+    "EmonHubHeatmiserInterfacer",
+    "EmonHubSofarSolarManInterfacer"
     #"EmonFroniusModbusTcpInterfacer"
 ]
